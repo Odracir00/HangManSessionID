07f7dc61a665f57e2400037e4c2b605396265ddc
Ricardo Santos <Ricardo Santos@Ricardo>
2015-09-30 12:36:42 +0100

    Segundo commit

--- VivaOBenfica/src/com/benfica/OlaBenfica.java
+++ VivaOBenfica/src/com/benfica/OlaBenfica.java
@@ -4,7 +4,8 @@ public class OlaBenfica {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("Viva o Benfica");
+		System.out.print("Viva o Benfica");
+		System.out.print("Viva o Benfica");
 	}
 
 }
